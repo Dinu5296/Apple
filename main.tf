c35171b5cd55f7aad49a7ee50e15febd729f4080
Dinu5296 <dineshkalluri@ttupskill.com>
2024-08-15 11:38:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-resource "aws_instance" "demo1" {
-  ami = "ami-0c8e23f950c7725b9"
-  instance_type = "t2.micro"
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg-${var.client}-${var.env}-${var.postfix}"
+  location = var.rg_location
 }
+resource "azurerm_storage_account" "storage_account1" {
+  name                     = "storageaccount456"
+  resource_group_name      = azurerm_resource_group.rg1.name
+  location                 = azurerm_resource_group.rg1.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
