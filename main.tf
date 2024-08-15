2c1c6f147c27dd7663e47aa7d5397154cba08994
Dinu5296 <dineshkalluri@ttupskill.com>
2024-08-15 11:53:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,37 @@
-resource "azurerm_resource_group" "rg1" {
-  name     = "rg-${var.client}-${var.env}-${var.postfix}"
-  location = var.rg_location
-}
-resource "azurerm_storage_account" "storage_account1" {
-  name                     = "storageaccount456"
-  resource_group_name      = azurerm_resource_group.rg1.name
-  location                 = azurerm_resource_group.rg1.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-hardcoded-bucket-name"
+  # Enable server-side encryption by default
+  bucket_encryption {
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+  }
+ 
+  # Enable versioning
+  versioning {
+    enabled = true
+  }
+ 
+  # Enable logging
+  logging {
+    target_bucket = "logging-target-bucket"
+    target_prefix = "log/"
+  }
+ 
+  # Public access block
+  public_access_block {
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
+ 
+  tags = {
+    Name        = "MyHardcodedS3Bucket"
+    Environment = "Dev"
+  }
 }
