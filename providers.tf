41d6f63fb4818cf7b56f261f8bb4b195cbde66ea
Dinu5296 <dineshkalluri@ttupskill.com>
2024-08-15 13:34:25 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "5.62.0"
+      source  = "hashicorp/aws"
+      version = "3.74.0"
     }
   }
-}
-
-provider "aws" {
-  # Configuration options
 }
