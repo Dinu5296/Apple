a30e7ad34d24fb7003d6922445c9dc9b3183b4c6
Dinu5296 <dineshkalluri@ttupskill.com>
2024-08-15 11:51:52 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,12 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.115.0"
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.62.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {
-
-  }
+provider "aws" {
   # Configuration options
 }
