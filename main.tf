39e9169615dffc106004f2bfaef1167b6723e6e2
Dinu5296 <dineshkalluri@ttupskill.com>
2024-08-15 12:46:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,38 +1,16 @@
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-hardcoded-bucket-name"
-  # Enable server-side encryption by default
-  bucket_encryption {
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-        }
-      }
-    }
-  }
+  bucket = "my-example-bucket"
+  acl    = "private"
  
-  # Enable versioning
   versioning {
     enabled = true
   }
  
-  # Enable logging
-  logging {
-    target_bucket = "logging-target-bucket"
-    target_prefix = "log/"
-  }
- 
-  /* Public access block
-  public_access_block {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-  }
-*/
- 
-  tags = {
-    Name        = "MyHardcodedS3Bucket"
-    Environment = "Dev"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
   }
 }
